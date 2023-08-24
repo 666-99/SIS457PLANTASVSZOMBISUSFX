fcc528146938ae15ac16a82619902b1be54c22a4
LourdesBS7 <130947228+LourdesBS7@users.noreply.github.com>
2023-08-24 00:53:22 -0300

    Update SpawnerDeSol.h

--- Plantas vs Zombis/SpawnerDeSol.h
+++ Plantas vs Zombis/SpawnerDeSol.h
@@ -2,18 +2,41 @@
 
 class SpawnerDeSol    //generador de soles
 {
+private:
 	//propiedades o atributos
+	//int cantidad;
+	
+protected:
 	int cantidad;
+	float posicionX;
+	float posicionY;
 	int intervaloGenerar;
-	int posicion;
 
-
+public:
 	//metodos o funciones
-	void activar();
-	void desactivar();
-	void generarSol();
-	void colisionObjetos();
-	void actualizar();
-	void configurarPosicion();
-};
 
+	//METODOS CONSTRUCTORES------------------------------------------------------
+	SpawnerDeSol();
+	SpawnerDeSol(int, float, float, int);
+
+	//METODOS ACCESORES (set y get)--------------------------------------------
+	void setCantidad(int _cantidad) { cantidad = _cantidad; }
+	int getCantidad() { return cantidad; }
+
+	void setPosicionX(float _posicionX) { posicionX = _posicionX; }
+	float getPosicionX() { return posicionX; }
+
+	void setPosicionY(float _posicionY) { posicionY = _posicionY; }
+	float getPosicionY() { return posicionY; }
+
+	void setIntervaloGenerar(int _intervaloGenerar) { intervaloGenerar = _intervaloGenerar; }
+	int getIntervaloGenerar() { return intervaloGenerar; }
+	
+	//METODOS PROPIOS O COMUNES--------------------------------------------
+	void activar();
+	void desactivar(int);
+	void generarSol(float, float, int, int);
+	void colisionObjetos();
+	void actualizar(int, float, float, int);
+	void configurarPosicion(float, float);
+};
