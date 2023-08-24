c8321eed88a47479a02f1f7c50bb17bf57badf34
HuaytaMa <143027761+HuaytaMa@users.noreply.github.com>
2023-08-24 10:54:57 -0400

    Update Zombi.h

--- Plantas vs Zombis/Zombi.h
+++ Plantas vs Zombis/Zombi.h
@@ -3,30 +3,58 @@
 
 using namespace std;
 
-class Zombi
+class Zombie
 {
 private:
-	//propiedades o atributos
+	int energia = 90;
+	int nivelFuria;
+	
+	string tipoZombie;
 	string nombre;
-	int tamaño;//(alto y ancho)
-	int Vida; //resistencia de 10 lanzaquisantes de plantas
-	int posicion;
-	string color;
-	char tipoZombi;
-	int rangoAtaque;
-	int danio;
-	char habilidad;
-	char recurso;
-	int recompensa;
+
+protected:
+	float posicionX;
+	float posicionY;
+	int direccionX;
+	int direccionY;
+	int velocidad;
 
 public:
-	//metodos o funciones
-	void mover();
-	void atacar();
-	void aumentarVelocidad();
-	void recibirDanio();
-	void atacarConRecursos(); //con lazamientos,.....
-	void realizarHabilidad(); //saltar
-	void morir();
-};
+	//Contructores
+	Zombie();
+	Zombie(string _nombre);
+	Zombie(string _nombre, int _energia, int _nivelFuria, int _velocidad, float _posicionX, float _posicionY, string _tipoZombie);
 
+	//Metodos accesores
+	void setEnergia(int _energia){ energia = _energia; }
+	int getEnergia() { return energia; }
+
+	void setNombre(string _nombre){ nombre = _nombre; }
+	string getNombre() { return nombre; }
+	
+	void setNivelFuria(int _nivelFuria){ nivelFuria = _nivelFuria; }
+	int getNivelFuria() { return nivelFuria; }
+
+	void setVelocidad(int _velocidad){ velocidad = _velocidad; }
+	int getVelocidad() { return velocidad; }
+
+	void setPosicionX(float _posicionX){ posicionX = _posicionX; }
+	float getPosicionX() { return posicionX; }
+
+	void setPosicionY(float _posicionY){ posicionY = _posicionY; }
+	float getPosicionY() { return posicionY; }
+
+	void setTipoZombie(string _tipoZombie){ tipoZombie = _tipoZombie; }
+	string getTipoZombie() { return tipoZombie; }
+
+
+
+	//Metodos propios
+	void moverse();
+	void moverse(float, float);
+	void moverse(float, float, int);
+
+	void atacar();
+	void morir();
+	void morder();
+};
