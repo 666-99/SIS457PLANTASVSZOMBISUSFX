4af6a147473b9d63aeb7ad0fcd2c37e8d4d71a7c
LourdesBS7 <130947228+LourdesBS7@users.noreply.github.com>
2023-08-24 00:57:00 -0300

    Update Principal.cpp

--- Plantas vs Zombis/Principal.cpp
+++ Plantas vs Zombis/Principal.cpp
@@ -6,6 +6,12 @@
 #include "Escenario.h"
 #include "BarraDeProgreso.h"
 #include "Proyectil.h"
+#include "SpawnerDeSol.h"
+#include "SpawnerDeMoneda.h"
+#include "SpawnerDeZombi.h"
+#include "GameManager.h"
+#include "GirasolGenerador.h"
+#include "JuegoPersonalizado.h"
 using namespace std;
 
 int main() {
@@ -22,6 +28,33 @@ int main() {
 
 	Proyectil guisante;
 	guisante.visualizarProyectil();
+
+	//-------------------------creando objetos------------
+	cout << "--------------OBJETOS---------------" << endl;
+	cout << endl;
+	cout << "--------------Objeto Spawner de Sol " << endl;
+	SpawnerDeSol Solesitos;
+	Solesitos.generarSol(2.0f, 12.0f, 4, 7);
+	cout << "La posicion X del sol es generado es: " << Solesitos.getPosicionX() << endl;
+	cout << "La posicion Y del sol es generado es: " << Solesitos.getPosicionY() << endl;
+	cout << endl;
+	cout << "-------------Objeto Game Manager----Juego------ " << endl;
+	GameManager JuegoPvZ;
+	JuegoPvZ.setInicio ("menu");
+	JuegoPvZ.setNivelActual (7);
+	JuegoPvZ.setDificultad("dificil");
+	JuegoPvZ.setPuntuacion (242);
+	JuegoPvZ.setJardin("noche");
+	JuegoPvZ.setSemillaDePlantas ("Menu_Plantas");
+	JuegoPvZ.setRegistroDeZombis ("Menu_Zombis");
+	JuegoPvZ.setEstadoJuego ("en-juego");
+	JuegoPvZ.setRecurso ("plata");
+	JuegoPvZ.setHerramienta ("puerta");
+	
+	cout << "El nivel del juego esta en: " << JuegoPvZ.getNivelActual() << endl;
+	cout << "La dificultad del juego esta en: " << JuegoPvZ.getDificultad() << endl;
+	JuegoPvZ.actualizarPuntuación();
+	cout << endl;
 
 		cout << "--------------CLASES HIJAS---------------" << endl;
 	cout << endl;
