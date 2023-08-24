1313826f2885179182a4141a928c930aff3cfb3f
LourdesBS7 <130947228+LourdesBS7@users.noreply.github.com>
2023-08-24 00:55:36 -0300

    Update GameManager.cpp

--- Plantas vs Zombis/GameManager.cpp
+++ Plantas vs Zombis/GameManager.cpp
@@ -1,5 +1,38 @@
 #include "GameManager.h"
 
+//METODOS CONSTRUCTORES------------------------------------------------------
+//GameManager::GameManager() {
+//
+//}
+
+GameManager::GameManager() {
+	inicio = "menu";
+	nivelActual = 7;
+	dificultad = 4;
+	puntuacion = 242;
+	jardin = "dia";
+	semillaDePlantas = "Menu_Plantas";
+	registroDeZombis = "Menu_Zombis";
+	estadoJuego = "pausado";
+	recurso = "oro";
+	herramienta = "pico";
+}
+
+GameManager::GameManager(int _nivelActual, string _dificultad, int _puntuacion, string _jardin, string _semillasDePlantas, string _estadoJuego, string _recurso, string _herramienta) {
+	//inicio = 'menu';
+	nivelActual = _nivelActual;
+	dificultad = _dificultad;
+	puntuacion = _puntuacion;
+	jardin = _jardin;
+	semillaDePlantas = _semillasDePlantas;
+	//registroDeZombis = "Menu_Zombis";
+	estadoJuego = _estadoJuego;
+	recurso = _recurso;
+	herramienta = _herramienta;
+}
+
+
+//METODOS PROPIOS O COMUNES--------------------------------------------
 void GameManager::datosDelJugador() {
 	//codigo
 }
@@ -20,7 +53,7 @@ void GameManager::actualizar() {
 	//codigo
 }
 
-void GameManager::actualizarPuntuación() {
+void GameManager::actualizarPuntuaciÃ³n() {
 	//codigo
 }
 
