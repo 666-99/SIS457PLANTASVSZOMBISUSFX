59d460e5ba46ac1be7ae01fe17bf4d7a4539deef
HuaytaMa <143027761+HuaytaMa@users.noreply.github.com>
2023-08-27 17:48:24 -0400

    Update Zombi.cpp

--- Plantas vs Zombis/Zombi.cpp
+++ Plantas vs Zombis/Zombi.cpp
@@ -1,61 +1,124 @@
-#include "Zombie.h"
+#include "Zombi.h"
 
-Zombie::Zombie()
-{
-	//energia = 100;
-	nivelFuria = 20;
-	velocidad = 30;
+//METODOS CONSTRUCTORES------------------------------------------------------
+
+Zombi::Zombi() {
+	nombre = "Zombi anonimo";
+	tamanioAlto = 7.0f;
+	tamanioAncho = 12.0f;
+	energiaVida = 100;
 	posicionX = 1000.0f;
 	posicionY = 540.0f;
-	tipoZombie = "Zombie normal";
-	nombre = "Zombie anonimo";
+	color = "verde y plomo";
+	tipoZombi = "Zombi Normal";
+	velocidad = 30;
+	nivelFuria = 20;
+	rangoAtaque = 7;
+	danio = 2;
+	habilidad = "excavar";
+	recurso = "pico";
 }
 
-Zombie::Zombie(string _nombre)
-{
-	//energia = 100;
-	nivelFuria = 20;
-	velocidad = 30;
-	posicionX = 1000.0f;
-	posicionY = 540.0f;
-	tipoZombie = "Zombie normal";
+Zombi::Zombi(string _nombre) {
+	//....................
 	nombre = _nombre;
+	tamanioAlto = 7.0f;
+	tamanioAncho = 12.0f;
+	energiaVida = 100;
+	posicionX = 1000.0f;
+	posicionY = 540.0f;
+	color = "verde y plomo";
+	tipoZombi = "Zombi Normal";
+	velocidad = 30;
+	nivelFuria = 20;
+	rangoAtaque = 7;
+	danio = 2;
+	habilidad = "excavar";
+	recurso = "pico";
 }
 
-Zombie::Zombie(string _nombre, int _energia, int _nivelFuria, int _velocidad, float _posicionX, float _posicionY, string _tipoZombie)
-{
-	energia = _energia;
-	nivelFuria = _nivelFuria;
+Zombi::Zombi(string _nombre, int _energiaVida, string _tipoZombi, int _velocidad, int _nivelFuria, int _habilidad) {
+	nombre = _nombre;
+	tamanioAlto = 7.0f;
+	tamanioAncho = 12.0f;
+	energiaVida = _energiaVida;
+	posicionX = 1000.0f;
+	posicionY = 540.0f;
+	color = "verde y plomo";
+	tipoZombi = _tipoZombi;
 	velocidad = _velocidad;
+	nivelFuria = _nivelFuria;
+	rangoAtaque = 7;
+	danio = 2;
+	habilidad = _habilidad;
+	recurso = "pico";
+}
+
+Zombi::Zombi(string _nombre, float _tamanioAlto, float _tamanioAncho, int _energiaVida, float _posicionX, float _posicionY, string _color, string _tipoZombi, int _velocidad, int _nivelFuria, string _habilidad) {
+	nombre = _nombre;
+	tamanioAlto = _tamanioAlto;
+	tamanioAncho = _tamanioAncho;
+	energiaVida = _energiaVida;
 	posicionX = _posicionX;
 	posicionY = _posicionY;
-	tipoZombie = _tipoZombie;
-	nombre = _nombre;
+	color = _color;
+	tipoZombi = _tipoZombi;
+	velocidad = _velocidad;
+	nivelFuria = _nivelFuria;
+	rangoAtaque = 7;
+	danio = 2;
+	habilidad = _habilidad;
+	recurso = "pico";
 }
 
-void Zombie::moverse()
-{
-	posicionX += velocidad;
-	posicionY += velocidad;
 
+//METODOS PROPIOS O COMUNES---------------------------------------------------
+
+void Zombi::moverse() {
+	//codigo
 }
-
-void Zombie::moverse(float _posicionSiguienteX, float _posicionSiguienteY)
-{
-}
-
-void Zombie::moverse(float _posicionFinalX, float _posicionFinalY, int _velocidad)
-{
-}
-
-void Zombie::atacar()
-{
-}
-
-void Zombie::morir() {
+void Zombi::moverse(float posicionSiguienteX, float posicionSiguienteY, int velocidadMover) {
 
 }
 
-void Zombie::morder()
-{
+void Zombi::atacar() {
+	//codigo
+}
+void Zombi::atacar(string _tipoZombi, int _velocidad, int _nivelFuria, int _rangoAtaque, int _danio, string _habilidad) {
+
+}
+
+void Zombi::aumentarVelocidad() {
+	//codigo
+}
+void Zombi::aumentarVelocidad(float _posicionX, float _posicionY, int _velocidad) {
+
+}
+
+void Zombi::recibirDanio() {
+	//codigo
+}
+void Zombi::recibirDanio(int _danioRecibido) {
+
+}
+
+void Zombi::atacarConRecursos() {
+	//codigo
+}
+void Zombi::atacarConRecursos(int _velocidad, string _habilidad, string _recurso) {
+
+}
+
+void Zombi::realizarHabilidad() {
+	//codigo
+}
+void Zombi::realizarHabilidad(int _velocidad, string _habilidad) {
+
+}
+
+void Zombi::morir() {
+	//codigo
+}
+void Zombi::morir(int _energiaVida) {
+
 }
